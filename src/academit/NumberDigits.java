8ec9393d6421aef37cb8e983b5e3270ff5450031
lihtenvald <lihtenvald95@mail.ru>
2024-03-07 22:47:53 +0700

    Задача «Цифры числа» выполнена

---
+++ src/academit/NumberDigits.java
@@ -0,0 +1,34 @@
+package academit;
+
+import java.util.Scanner;
+
+public class NumberDigits {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите целое число:");
+        int number = scanner.nextInt();
+
+        int digitsSum = 0;
+        int evenDigitsSum = 0;
+        int maxDigit = 0;
+
+        while (number != 0) {
+            int lastDigit = number % 10;
+            digitsSum += lastDigit;
+
+            if (lastDigit % 2 != 0) {
+                evenDigitsSum += lastDigit;
+            }
+            if (lastDigit > maxDigit) {
+                maxDigit = lastDigit;
+            }
+            number /= 10;
+        }
+
+        System.out.println("Сумма цифр целого числа: " + digitsSum);
+        System.out.println("Сумма нечетных цифр целого числа: " + evenDigitsSum);
+        System.out.println("Максимальная цифра целого числа: " + maxDigit);
+    }
+}
